833fd2d49443eb4cfbac00b42a0805543cc18fc8
Luis <101060519+LuisFun@users.noreply.github.com>
2024-07-22 19:40:11 +0900

    feat: home nav

---
+++ src/components/nav/data.ts
@@ -0,0 +1,52 @@
+export const navData = [
+  {
+    svg: 'atk-crit' as const,
+    text: 'Check Ideal',
+    href: '/atk-crit/',
+  },
+  {
+    svg: 'team' as const,
+    text: 'Team Builder',
+    href: '/team-builder/',
+  },
+  {
+    svg: 'character' as const,
+    text: 'Showcase',
+    href: '/showcase/',
+  },
+  {
+    svg: 'avg-dmg' as const,
+    text: 'Calculator',
+    href: '/avg-dmg/',
+  },
+  {
+    svg: 'wish' as const,
+    text: 'Wish Banner',
+    href: '/wish-banner/',
+  },
+  {
+    svg: 'analytics' as const,
+    text: 'Statistics',
+    href: '/showcase-analytics/',
+  },
+  {
+    svg: 'book' as const,
+    text: 'Talent Materials',
+    href: '/talent-materials/',
+  },
+  {
+    svg: 'dice' as const,
+    text: 'Randomizer',
+    href: '/team-randomizer/',
+  },
+  {
+    svg: 'circlet' as const,
+    text: 'Artifacter',
+    href: '/artifacter/',
+  },
+  {
+    svg: 'player-card' as const,
+    text: 'Player Card',
+    href: '/player-card-templates/',
+  },
+]
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -5,16 +5,19 @@ export const ui = {
     label: 'English',
     'game.title': 'Genshin Impact',
     'footer.unofficial': 'Paimon+ is not affiliated to miHoYo.',
+    'home.title': 'Genshin Impact Online Tools',
     ts: 'ts',
   },
   ja: {
     label: '日本語',
     'game.title': '原神',
     'footer.unofficial': 'Paimon+は非公式のwebサイトです。',
+    'home.title': '原神 webツール',
   },
   ko: {
     label: '한국어',
     'game.title': '원신',
     'footer.unofficial': 'Paimon+는 비공식 웹사이트입니다.',
+    'home.title': '원신 웹 툴',
   },
 } as const

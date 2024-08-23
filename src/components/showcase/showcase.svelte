29a845b79c2f671e7781421802f9f2681d3acf4f
Luis <101060519+LuisFun@users.noreply.github.com>
2024-08-23 19:06:18 +0900

    feat: add ui

--- src/components/showcase/showcase.svelte
+++ src/components/showcase/showcase.svelte
@@ -3,19 +3,32 @@
 import type { ApiData } from '@components/api'
 import Icon from '@components/icon.svelte'
 import Uid from '@components/uid.svelte'
+import type { Lang } from '@i18n/utils'
+import Card from './card.svelte';
+
+export let lang: Lang
 
 let uidData: ApiData | undefined
 let status = 0
 </script>
 
 <Uid bind:uidData bind:status />
+{#if uidData?.avatarInfoList?.[0]}
+<div class="overflow-x-auto">
+  <div class="w-[768px] md:w-[1024px] rounded-lg overflow-hidden">
+    <Card {lang} avatarInfo={uidData.avatarInfoList[0]} />
+  </div>
+</div>
+{/if}
 {#if uidData}
 <div>
   <div>{uidData.uid}</div>
   {#if uidData.avatarInfoList}
-  <div class="flex">
+  <div class="flex flex-wrap">
     {#each uidData.avatarInfoList as avatar}
+    <div class="w-16">
       <Icon id={avatar.avatarId} ui="avatar" />
+    </div>
     {/each}
   </div>
   {/if}

df32fbc1f0193599c8ec62be27aa270b51ff5559
Luis <101060519+LuisFun@users.noreply.github.com>
2024-08-26 14:42:03 +0900

    fix: cache kv -> d1

--- functions/api/uid/[uid].ts
+++ functions/api/uid/[uid].ts
@@ -3,7 +3,7 @@ import { API_VER, uidTest } from '../../../src/components/api'
 import type { ApiData, EnkaApi, Reliquary, Weapon } from '../../../src/components/api'
 
 type Env = {
-  kv: KVNamespace
+  //kv: KVNamespace
   showcase: D1Database
   statistical: D1Database
 }
@@ -13,46 +13,59 @@ export const onRequestGet: PagesFunction<Env, 'uid'> = async ctx => {
   if (ctx.request.headers.get('sec-fetch-site') !== 'same-origin') return resStatus(403)
   if (typeof uid !== 'string' || !uidTest(uid)) return resStatus(400)
 
+  const KEY_STATUS = 'enka-status'
+
   //********** cache section **********
   // cache init
   const cache = (caches as unknown as CacheStorage).default
   const reqUrl = new URL(ctx.request.url)
   const cacheKey = reqUrl.origin + reqUrl.pathname
-  // kv key name
-  const KEY_STATUS = '0-enka-status'
-  const KEY_SHOWCASE = '0-showcase-uids'
 
-  // get cache, kv
+  // get db, cache
   const cacheData = await Promise.all([
-    ctx.env.kv.get(uid),
-    ctx.env.kv.get(KEY_STATUS, { cacheTtl: 180 }), // 3m
-    ctx.env.kv.get(KEY_SHOWCASE, { cacheTtl: 180 }), // 3m
+    (async () => {
+      const db = ctx.env.showcase
+      const results = (
+        await db.batch([
+          db.prepare('SELECT * FROM cache_uid WHERE uid = ? LIMIT 1').bind(uid),
+          db.prepare('SELECT * FROM key_value WHERE key = ? LIMIT 1').bind(KEY_STATUS),
+          db.prepare('SELECT name FROM sqlite_master WHERE type="table"'),
+        ])
+      ).map(e => e.results)
+      const cache = results[0][0] as { uid: string; data: string; updated_at: number } | null
+      const status = results[1][0] as { key: string; value: string; updated_at: number }
+      const uids = (results[2] as { name: string }[]).map(e => e.name.substring(1))
+      return { cache, status, uids }
+    })(),
     cache.match(cacheKey),
   ])
-  const uidCache: ApiData | undefined = await cacheData[3]?.json()
+  const uidCache: ApiData | undefined = await cacheData[1]?.json()
+  const isValid = (result: { updated_at: number } | undefined, sec: number) =>
+    !!result && result.updated_at + sec * 1e3 < Date.now()
 
   // response error
-  if (cacheData[0] === '400' || cacheData[0] === '404') return resError(cacheData[0], uidCache)
-  if (cacheData[1]) return resError(cacheData[1], uidCache) // 424/429/500/503
+  const uidStatus = cacheData[0].cache.data
+  if (isValid(cacheData[0].cache, 180) && (uidStatus === '400' || uidStatus === '404'))
+    return resError(uidStatus, uidCache)
+  if (isValid(cacheData[0].status, 180)) return resError(cacheData[0].status.value, uidCache) // 424/429/500/503
 
   // response cache
-  if (cacheData[0]) {
-    const uidKv: ApiData = JSON.parse(cacheData[0])
-    const cacheTime = Math.ceil(uidKv.timestamp / 1000 + uidKv.ttl - Date.now() / 1000)
-    if (cacheTime > 0) await logCacheTime(ctx.env, cacheTime) //********** cache time log **********
-    if (cacheTime > 0) return resJson203(304, { ...uidKv, ttl: cacheTime }, cacheTime)
+  if (isValid(cacheData[0].cache, 60)) {
+    const uidData: ApiData = JSON.parse(cacheData[0].cache.data)
+    //const uidAllData = getDBShowcase(ctx.env, uid)
+    const age = Math.ceil(uidData.timestamp / 1000 + uidData.ttl - Date.now() / 1000)
+    if (age > 0) await logCacheTime(ctx.env, age) //********** cache time log **********
+    if (age > 0) return resJson203(304, uidData, age)
   }
   // response force cache
-  if (ctx.request.headers.get('cache-control') === 'force-cache' && uidCache)
-    return resJson203(304, { ...uidCache, ttl: 0 }, 0)
+  if (ctx.request.headers.get('cache-control') === 'force-cache' && uidCache) return resJson203(304, uidCache, 0)
 
   //if (uid) return resJson({uid}, 200, 60)
 
   //********** fetch section **********
-  const uids: number[] = JSON.parse(cacheData[2])
   const uidData = await Promise.all([
     fetch(`https://enka.network/api/uid/${uid}`, { headers: { 'User-Agent': 'Paimon+@luis.fun/1.0' } }),
-    //fetchDB(c, uid),
+    //getDBShowcase(ctx.env, uid),
   ])
 
   // error response
@@ -62,13 +75,13 @@ export const onRequestGet: PagesFunction<Env, 'uid'> = async ctx => {
     case 429:
     case 500:
     case 503:
-      await ctx.env.kv.put(KEY_STATUS, status.toString(), { expirationTtl: 180 }) // 3m
+      await putDB('kv', ctx.env, KEY_STATUS, status.toString(), Date.now())
       //await sendDiscord(status)
       console.log(`fetch enka.status: ${status}, ${uid}`)
       return resError(status, uidCache)
     case 400:
     case 404:
-      await ctx.env.kv.put(uid, status.toString(), { expirationTtl: 60 }) // 1m
+      await putDB('uid', ctx.env, uid, status.toString(), Date.now())
       return resError(status, uidCache)
   }
   // other error
@@ -82,9 +95,8 @@ export const onRequestGet: PagesFunction<Env, 'uid'> = async ctx => {
   // save
   ctx.waitUntil(
     Promise.all([
-      ctx.env.kv.put(uid, JSON.stringify(json), { expirationTtl: 60 }), // 1m
       saveCache(cache, cacheKey, res),
-      saveShowcase(ctx.env, uid, json, uids),
+      saveShowcase(ctx.env, uid, json, cacheData[0].uids),
       saveStatistical(ctx.env, uid, json),
     ]),
   )
@@ -96,22 +108,37 @@ export const onRequestGet: PagesFunction<Env, 'uid'> = async ctx => {
 //********** cache time log **********
 const logCacheTime = async (env: Env, time: number) => {
   const t = 60 - time
-  const key = '0-cache-time'
-  const log = Number(await env.kv.get(key))
-  if (!log) await env.kv.put(key, t.toString())
-  if (log > t) await env.kv.put(key, t.toString())
+  const key = 'cache-time'
+  const log = Number((await getDB('kv', env, key)).value)
+  if (!log || log > t) await putDB('kv', env, key, t.toString(), Date.now())
 }
 
 const resStatus = (status: number) => new Response(null, { status })
 const resError = (status: string | number, uidCache: ApiData | undefined) =>
   uidCache ? resJson203(status, uidCache, uidCache.ttl) : resStatus(Number(status))
-const resJson203 = (status: string | number, uidCache: ApiData | undefined, age: number) =>
-  resJson({ ...uidCache, status: Number(status) }, 203, age)
+const resJson203 = (status: string | number, apiData: ApiData | undefined, age: number) =>
+  resJson({ ...apiData, status: Number(status) }, 203, age)
 const resJson = (json: unknown, status: number, age: number) =>
   new Response(JSON.stringify(json), {
     status,
     headers: { 'Content-Type': 'application/json', 'Cache-Control': `max-age=${age}` },
   })
+
+const getDB = async (type: 'kv', env: Env, key: string) => {
+  const db = env.showcase
+  const query = 'SELECT * FROM key_value WHERE key = ? LIMIT 1'
+  return await db.prepare(query).bind(key).first<{ key: string; value: string; updated_at: number }>()
+}
+const putDB = async (type: 'kv' | 'uid', env: Env, key: string, value: string, timestamp: number) => {
+  const db = env.showcase
+  const query =
+    type === 'uid'
+      ? 'REPLACE INTO cache_uid (uid, updated_at, data) VALUES(?, ?, ?)'
+      : 'REPLACE INTO key_value (key, updated_at, value) VALUES(?, ?, ?)'
+  return await db.prepare(query).bind(key, timestamp, value).all<undefined>()
+}
+
+const getDBShowcase = async (env: Env, uid: string) => {}
 
 const saveCache = async (cache: CacheStorage['default'], cacheKey: string, res: Response) => {
   res.headers.append('Cache-Control', 's-maxage=15552000') // 180day
@@ -119,10 +146,19 @@ const saveCache = async (cache: CacheStorage['default'], cacheKey: string, res: 
   await cache.put(cacheKey, res.clone())
 }
 
-const saveShowcase = async (env: Env, uid: string, json: ApiData, uids: number[]) => {
-  if (!uids?.includes(Number(uid))) return
+const saveShowcase = async (env: Env, uid: string, json: ApiData, uids: string[]) => {
   const db = env.showcase
   const { timestamp } = json
+  // cache
+  await db.batch([
+    db
+      .prepare('REPLACE INTO cache_uid (uid, updated_at, data) VALUES(?, ?, ?)')
+      .bind(uid, timestamp, JSON.stringify(json)),
+    db.prepare('DELETE FROM cache_uid WHERE updated_at < ?').bind(timestamp - 180_000),
+  ])
+  // save
+  if (!uids?.includes(uid)) return
+  //********************************
 }
 
 const saveStatistical = async (env: Env, uid: string, json: ApiData) => {
@@ -130,20 +166,22 @@ const saveStatistical = async (env: Env, uid: string, json: ApiData) => {
   if (!json.avatarInfoList) return
   const db = env.statistical
   const { timestamp } = json
-  const tableList = (await db.prepare('SELECT name FROM sqlite_master WHERE type="table"').all()).results?.map(
-    e => e.name as string,
+  const tableList = (await db.prepare('SELECT name FROM sqlite_master WHERE type="table"').raw<{ name: string }>()).map(
+    e => e.name,
   )
   const idList = json.avatarInfoList.map(e => `_${e.avatarId}`)
   const diffId = idList.filter(x => tableList.indexOf(x) === -1)
   // テーブル生成
   if (diffId[0])
     await db.batch([
-      ...diffId.map(e => db.prepare(`CREATE TABLE IF NOT EXISTS ${e} (uid INT PRIMARY KEY, timestamp INT, data TEXT)`)),
+      ...diffId.map(e =>
+        db.prepare(`CREATE TABLE IF NOT EXISTS ${e} (uid INT PRIMARY KEY, updated_at INT, data TEXT)`),
+      ),
     ])
   await db.batch([
     // プレイヤーデータ保存
     db
-      .prepare('REPLACE INTO player (uid, timestamp, data) VALUES(?1, ?2, ?3)')
+      .prepare('REPLACE INTO player (uid, updated_at, data) VALUES(?, ?, ?)')
       .bind(uid, timestamp, JSON.stringify(json.playerInfo)),
     // Characterデータ保存
     ...json.avatarInfoList
@@ -165,7 +203,7 @@ const saveStatistical = async (env: Env, uid: string, json: ApiData) => {
       })
       .map(e =>
         db
-          .prepare(`REPLACE INTO _${e.avatarId} (uid, timestamp, data) VALUES(?1, ?2, ?3)`)
+          .prepare(`REPLACE INTO _${e.avatarId} (uid, updated_at, data) VALUES(?, ?, ?)`)
           .bind(uid, timestamp, JSON.stringify(e)),
       ),
   ])

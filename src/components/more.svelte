5a6fa37cc3922a31975a38f4d1c326e326eb9f15
Luis <101060519+LuisFun@users.noreply.github.com>
2024-08-27 14:35:45 +0900

    fix: uid-next

---
+++ src/components/more.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import Svg from '@components/svg.svelte'
+export let mt0 = false
+</script>
+
+<div class="collapse grid-rows-[0fr_auto] rounded-none">
+  <input type="checkbox" class="!row-start-2 {mt0 ? "mt-0" :"mt-3"} min-h-[unset]" aria-label="more" />
+  <div class="collapse-title row-start-2 flex justify-center items-center {mt0 ? "mt-0" :"mt-3"} p-0 pb-3 min-h-[unset]">
+    <Svg icon="angle-down" class="svg-down w-4" />
+    <Svg icon="angle-up" class="svg-up hidden w-4" />
+  </div>
+  <div class="collapse-content row-start-1 visible overflow-hidden !p-0">
+    <slot />
+  </div>
+</div>
--- src/components/uid-next.svelte
+++ src/components/uid-next.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 import { fetchUid, uidTest } from '@components/api'
 import type { ApiData } from '@components/api'
-import Icon from './icon.svelte'
+import Icon from '@components/icon.svelte'
+import Svg from '@components/svg.svelte';
+import More from '@components/more.svelte';
+import Dialog from '@components/dialog.svelte'
+import type { HTMLButtonAttributes } from 'svelte/elements'
 
 export let uid: number | undefined
 export let apiData: ApiData | undefined
@@ -14,17 +18,37 @@ const clickHandler = async () => {
   apiData = res.json
   status = res.status
 }
+
+// tsエラー回避用
+const onclick: HTMLButtonAttributes = {
+  // @ts-expect-error
+  onclick: 'uid_log.showModal()',
+}
 </script>
 
-<div class="flex justify-between items-center h-12 p-1">
+<div class="flex justify-between items-center h-10 px-1">
   <div class="flex items-center">
     <div class="w-10 h-10 mr-4 flex justify-center items-center">
       <Icon id={apiData?.playerInfo.profilePicture.id || 2} ui="circle" />
     </div>
     <div>{apiData?.playerInfo.nickname}</div>
   </div>
-  <div class="flex items-center">
-    <input type="number" placeholder="UID" bind:value={uid} class="input input-bordered w-full max-w-xs bg-neutral" />
-    <button class="btn btn-neutral" on:click={clickHandler} {disabled}>fetch</button>
+  <div class="flex items-center input input-bordered px-1 h-8 bg-neutral rounded-full">
+    <button class="btn btn-neutral p-1 min-h-6 w-6 h-6 rounded-full" {...onclick}>
+      <Svg icon="clock-rotate-left" />
+    </button>
+    <input type="number" placeholder="UID" bind:value={uid} class="no-spin w-24 text-center leading-8" />
+    <button class="btn btn-neutral p-0.5 min-h-6 w-6 h-6 rounded-full" on:click={clickHandler} {disabled}>
+      <Svg icon="angle-right" height="100%" />
+    </button>
   </div>
 </div>
+<More mt0>
+  aaa
+</More>
+<Dialog id="uid_log">
+  <div class="h-12">
+  uid
+  log
+  </div>
+</Dialog>

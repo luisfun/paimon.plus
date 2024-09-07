df350e6878ad32bdc6ba1ead71ebc1318edffa9e
Luis <101060519+LuisFun@users.noreply.github.com>
2024-09-07 14:49:12 +0900

    feat: loading ring

--- src/components/uid-init.svelte
+++ src/components/uid-init.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { fetchUid, uidTest } from '@components/api'
 import type { ApiData } from '@components/api'
+import Svg from '@components/svg.svelte'
 import { onMount } from 'svelte'
 
 export let uid: number | undefined
@@ -8,6 +9,7 @@ export let apiData: ApiData | undefined
 export let status: number
 let disabled = true
 $: disabled = !uidTest(uid)
+let isInitInput = false
 
 onMount(async () => {
   const lsUid = localStorage.getItem('uid')
@@ -17,6 +19,7 @@ onMount(async () => {
     apiData = res.json
     status = res.status
   }
+  isInitInput = true
 })
 
 const clickHandler = async () => {
@@ -26,7 +29,15 @@ const clickHandler = async () => {
 }
 </script>
 
-<div>
-  <input type="number" placeholder="UID" bind:value={uid} class="input input-bordered w-full max-w-xs bg-neutral" />
-  <button class="btn btn-neutral" on:click={clickHandler} {disabled}>fetch</button>
+<div class="flex justify-center mt-[15svh]">
+  {#if isInitInput}
+  <div class="flex items-center input input-bordered px-1 bg-neutral rounded-full">
+    <input type="number" placeholder="UID" bind:value={uid} class="no-spin w-48 text-center text-2xl leading-[3rem]" />
+    <button class="btn btn-neutral p-0.5 min-h-8 w-8 h-8 rounded-full" on:click={clickHandler} {disabled}>
+      <Svg icon="angle-right" height="100%" />
+    </button>
+  </div>
+  {:else}
+  <div class="loading loading-ring w-24" />
+  {/if}
 </div>

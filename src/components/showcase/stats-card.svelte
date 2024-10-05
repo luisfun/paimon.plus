c2985fb211bf16a4adab700afbe7469559d436b7
Luis <101060519+LuisFun@users.noreply.github.com>
2024-10-06 07:05:42 +0900

    chore: save

--- src/components/showcase/stats-card.svelte
+++ src/components/showcase/stats-card.svelte
@@ -1,13 +1,22 @@
+<svelte:options runes={true} />
 <script lang="ts">
 import type { AvatarInfo } from '@components/api'
 import type { Lang } from '@i18n/utils'
 import RawSubStats from '@manual/showcase-sub-stats.json'
 import Card from './card.svelte'
+import { useTranslations } from '@i18n/utils'
+const en = useTranslations("en")
+type SubStat = 'CRIT' | 'HP' | 'ATK' | 'DEF' | 'ER' | 'EM'
+const SubStats = RawSubStats as Record<keyof typeof RawSubStats, SubStat[]>
+const subStats = (name: string) => SubStats[name as keyof typeof RawSubStats] || SubStats["default"]
 
-const SubStats = RawSubStats as Record<keyof typeof RawSubStats, ('CRIT' | 'HP' | 'ATK' | 'DEF' | 'ER' | 'EM')[]>
+type Props = {
+  lang: Lang
+  avatarInfo: AvatarInfo | undefined
+}
+const { lang, avatarInfo }: Props = $props()
 
-export let lang: Lang
-export let avatarInfo: AvatarInfo | undefined
+let subMarks = $state(subStats(en(avatarInfo?.avatarId || 0, "avatar")))
 </script>
 
 {#if avatarInfo}

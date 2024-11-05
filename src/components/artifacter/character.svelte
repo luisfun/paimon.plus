27c658625ea0f3011d155ee7f2f02ab961d7ec0f
Luis <101060519+LuisFun@users.noreply.github.com>
2024-11-06 06:27:11 +0900

    fix: ios

--- src/components/artifacter/character.svelte
+++ src/components/artifacter/character.svelte
@@ -32,7 +32,7 @@ let scoreType = $state<ScoreType>(scoreTypeMenuItems[0])
 </div>
 <div class="flex justify-around sm:justify-evenly">
   <button class="btn btn-sm btn-primary leading-8" onclick={() => imageDownload(canvas, "card")}>
-    <Svg icon="download" class="py-1.5" height="100%" />{t("card.download")}
+    <Svg icon="download" class="py-1.5 inline-block" height="100%" />{t("card.download")}
   </button>
   <div class="flex justify-center">
     <select bind:value={scoreType} class="select select-bordered select-primary select-sm w-full max-w-xs mr-2 sm:mr-4">

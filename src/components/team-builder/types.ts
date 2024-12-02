9cc37274299d9eafa2c333fe3e6948e8b1eee523
Luis <101060519+LuisFun@users.noreply.github.com>
2024-12-02 18:50:16 +0900

    feat: builder-data

--- src/components/team-builder/types.ts
+++ src/components/team-builder/types.ts
@@ -2,12 +2,14 @@ export type Name = string
 type Roll = 'main' | 'sub' | 'support' | 'healer'
 export type Elem = '4elem' | 'Pyro' | 'Hydro' | 'Cryo' | 'Electro' | 'Dendro' | 'Anemo' | 'Geo'
 type Dmg = 'normal' | 'charge' | 'plunge' | 'skill' | 'burst' | 'element'
-type Stat = 'HP' | 'ATK' | 'DEF'
+type Stat = 'HP' | 'ATK' | 'DEF' | 'EM'
+type Trigger = 'normal'
 type Scope = {
   roll?: ('main' | 'sub')[]
   elem?: Elem[]
   dmg?: Dmg[]
   stat?: Stat[]
+  trigger?: Trigger[]
   exclude?: (Roll | Elem | Dmg)[]
 }
 type Member = Name | Scope
@@ -20,6 +22,7 @@ export type Avatar = {
   burstDep?: number // 爆発依存度 探索には-1点分影響させる // + max(点数, max(星4キャラ数, 星5キャラ数)×0.1) - 点数 // 爆発依存有りで40,60,80族 -> 1,2,3 辺りで // 3以上はscore-1 同族ありでscore-0
   dmg?: Dmg[] // roll: main, sub のダメージタイプ
   stat?: Stat[] // default: "ATK" roll: main, sub のダメージタイプ
+  trigger?: Trigger[] // main 以外の発動条件
   explor?: Explor // × minmax(0, (星5キャラ数-10)×0.1,1)
   coop?: {
     score: number
